6e38e0ffd04246c870c7bcb8f615e4ea2f6c4da4
Maksim Baranov <oooinfo@mail.ru>
2024-05-21 17:35:13 +0300

    add react-router

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from "react-router-dom";
 import BaseLayout from './layouts/BaseLayout'
+import { appRouter } from './appRouter';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BaseLayout/>
+    <RouterProvider router={appRouter}/>
 )
