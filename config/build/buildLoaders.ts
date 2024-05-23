dcdf8a9c1d470704a3396010e91661888cc7c770
Maksim Baranov <oooinfo@mail.ru>
2024-05-23 14:32:20 +0300

    add assets loader and svgr plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,14 +2,41 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { ModuleOptions } from "webpack";
 import { BuildOptions } from "./types/types";
 
-export function buildLoaders(options:BuildOptions):ModuleOptions['rules']{
+export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development';
+
+    const assetLoader = {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    }
+
+    const svgrLoader = {
+        test: /\.svg$/i,
+        use: [
+            {
+                loader: '@svgr/webpack',
+                options: {
+                    icon: true,
+                    svgoConfig: {
+                        plugins: [
+                            {
+                                name: 'convertColors',
+                                params: {
+                                    currentColor: true,
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        ],
+    }
 
     const cssLoader = {
         test: /\.css$/i,
         use: [
-        isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 
-        "css-loader"],
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            "css-loader"],
     }
 
     const tsLoader = {
@@ -19,7 +46,9 @@ export function buildLoaders(options:BuildOptions):ModuleOptions['rules']{
     }
 
     return [
+        assetLoader,
         cssLoader,
-        tsLoader
+        tsLoader,
+        svgrLoader
     ]
 }
