d0525168272ed4526e45fec624bfa63b59223794
Maksim Baranov <oooinfo@mail.ru>
2024-05-23 12:22:23 +0300

    add bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,15 +2,25 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { Configuration } from "webpack";
 import { BuildOptions } from "./types/types";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
-export function buildPlugins({mode, paths}:BuildOptions):Configuration["plugins"]{
+export function buildPlugins({ mode, paths }: BuildOptions): Configuration["plugins"] {
     const isProd = mode === 'production';
 
-    return [
+    const plugins: Configuration["plugins"] = [
         new HtmlWebpackPlugin({ template: paths.html }),
-        isProd && new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css'
-        })
     ]
+
+    if (isProd) {
+        plugins.push(
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css'
+            })
+        )
+        plugins.push(
+            new BundleAnalyzerPlugin()
+        )
+    }
+    return plugins
 }
