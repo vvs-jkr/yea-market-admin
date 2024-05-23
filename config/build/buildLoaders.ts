3707f476cd2c527e9baee4edbef71850324283e9
Maksim Baranov <oooinfo@mail.ru>
2024-05-23 16:03:51 +0300

    add hot module replacement

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,7 @@
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { ModuleOptions } from "webpack";
 import { BuildOptions } from "./types/types";
+import ReactRefreshTypeScript from "react-refresh-typescript";
 
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development';
@@ -40,9 +41,19 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     }
 
     const tsLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
         exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: [
+            {
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true,
+                    getCustomTransformers: () => ({
+                        before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
+                    }),
+                }
+            }
+        ]
     }
 
     return [
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,13 +3,21 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { Configuration } from "webpack";
 import { BuildOptions } from "./types/types";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin"
 
 export function buildPlugins({ mode, paths }: BuildOptions): Configuration["plugins"] {
     const isProd = mode === 'production';
+    const isDev = mode === 'development';
 
     const plugins: Configuration["plugins"] = [
         new HtmlWebpackPlugin({ template: paths.html }),
     ]
+
+    if (isDev) {
+        plugins.push(
+            new ReactRefreshWebpackPlugin()
+        )
+    }
 
     if (isProd) {
         plugins.push(
