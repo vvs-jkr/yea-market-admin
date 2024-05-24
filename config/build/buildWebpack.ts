02706b39fca8f573eb88304577a11ea7f16ad302
Maksim Baranov <oooinfo@mail.ru>
2024-05-24 11:32:59 +0300

    check devtools source-map

--- config/build/buildWebpack.ts
+++ config/build/buildWebpack.ts
@@ -21,7 +21,7 @@ export function buildWebpack(options:BuildOptions):webpack.Configuration{
             rules: buildLoaders(options)
         },
         resolve: buildResolvers(options),
-        devtool: isDev ? 'inline-source-map' : false,
+        devtool: isDev ? 'inline-source-map' : 'source-map',
         devServer: isDev ? buildDevServer(options) : undefined,
     }
 }
