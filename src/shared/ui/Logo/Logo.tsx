5e2ae6363b56b0a9ef627b0328dc8b9680ecb24f
Maksim Baranov <oooinfo@mail.ru>
2024-05-30 16:46:05 +0300

    add Logo ui component

---
+++ src/shared/ui/Logo/Logo.tsx
@@ -0,0 +1,10 @@
+import { LogoProps } from './logo.model'
+import logo from './assets/logo.svg'
+import styles from './styles.module.css'
+
+const Logo = ({ onClick, ...props }: LogoProps) => {
+  const SvgLogo = logo
+  return <SvgLogo className={styles.logo} onClick={onClick} {...props} />
+}
+
+export default Logo
