8733e65736a37f4039940545845a71a55a02472c
Maksim Baranov <oooinfo@mail.ru>
2024-06-02 15:03:05 +0300

    add AdminLoginForm

--- src/shared/ui/Button/button.model.ts
+++ src/shared/ui/Button/button.model.ts
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export type ButtonVariantTypes = 'light' | 'dark' | 'light-outline' | 'dark-outline'
+export type ButtonVariantTypes = 'light' | 'dark' | 'light-outline' | 'dark-outline' | 'light-form' | 'dark-form'
 
 export interface ButtonProps extends React.ComponentProps<'button'> {
   label?: string
