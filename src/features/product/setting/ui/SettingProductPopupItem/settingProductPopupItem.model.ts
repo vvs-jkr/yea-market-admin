1442be910956b9be86dbf3aee8c549fee1d1f920
Beck Yuldashev <yuldashev.bek@gmail.com>
2024-06-04 22:00:00 +0300

    [feature/RF-159] make a product popup

---
+++ src/features/product/setting/ui/SettingProductPopupItem/settingProductPopupItem.model.ts
@@ -0,0 +1,7 @@
+import { IconType } from '@/shared/ui/Icon/icon.model'
+
+export interface SettingProductPopumItemProps {
+  icon: IconType;
+  label?: string;
+  isActive?: boolean;
+}
--- src/shared/ui/Icon/assets/index.ts
+++ src/shared/ui/Icon/assets/index.ts
@@ -4,15 +4,19 @@ import ChevronRight from './ChevronRight.svg'
 import ChevronUp from './ChevronUp.svg'
 import Heart from './Heart.svg'
 import Search from './Search.svg'
-import Checkmark from './Checkmark.svg';
-import Close from './Close.svg';
-import Box from './Box.svg';
-import Contacts from './Contacts.svg';
-import Logout from './Logout.svg';
-import Settings from './Settings.svg';
-import Trending from './Trending.svg';
+import Checkmark from './Checkmark.svg'
+import Close from './Close.svg'
+import Box from './Box.svg'
+import Contacts from './Contacts.svg'
+import Logout from './Logout.svg'
+import Settings from './Settings.svg'
+import Trending from './Trending.svg'
 import Download from './Download.svg'
 import DottedMenu from './DottedMenu.svg'
+import ClickableArea from './ClickableArea.svg'
+import HardDrive from './HardDrive.svg'
+import Print from './Print.svg'
+import Trash from './Trash.svg'
 
 const icons = {
   ChevronDown,
@@ -29,7 +33,11 @@ const icons = {
   Settings,
   Trending,
   Download,
-  DottedMenu
+  DottedMenu,
+  ClickableArea,
+  HardDrive,
+  Print,
+  Trash
 }
 
 export default icons
